cdb1213f5d1b3e01b2a85a6a6eb74f603da69a79
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-10-26 16:47:39 +0300

    read and add methods is done

--- HW2/database.js
+++ HW2/database.js
@@ -7,24 +7,27 @@ class Course {
     }
 
     toString () {
-        return this.code
+        return this.code+''
     }
 }
 class Student {
-    constructor () {
-
+    constructor (id, name, gpa, courses) {
+        this.id = id || ''
+        this.name = name || ''
+        this.gpa = gpa || ''
+        this.courses = courses || []
     }
     toString () {
-        return
+        return this.id+''
     }
 }
-
 
 class Database {
     constructor () {
         this.courses = new Map()
         this.students = new Map()
         this.readCourses()
+        this.readStudents()
     }
 
     readCourses () {
@@ -32,30 +35,68 @@ class Database {
         fetch(url)
         .then(res => res.text()) 
         .then(res => [
-            this.addStudents(res)
-        ]) 
-        
-    }
-    toString (txt) {
-        return console.log(txt)
+            this.addCourses(res)
+        ])
     }
 
-    addStudents(txt) {
-        let msg = txt.length+" chars, ";
-        let a = txt.split("\n");
-        msg += a.length+" lines, ";
+    readStudents () {
+        var url = "https://maeyler.github.io/JS/data/Students.txt"
+        fetch(url)
+        .then(res => res.text())
+        .then(res => [
+            this.addStudents(res)
+        ])
+    }
+
+    addCourses(txt) {
+        let msg = 'Courses: ' + txt.length + ' chars, ';
+        let a = txt.split('\n');
+        msg += a.length + ' lines';
+        console.log(msg)
+
+        //create course object
         const courseList = a.map(item => {
-            let bla = item.split('\t')
-            const courses = new Course(bla[0], bla[1], bla[2], bla.slice(3))
-            const a = ['code', bla[0]]
-            this.courses.set('code', bla[0])
-            this.courses.set('time', bla[1])
-            this.courses.set('date', bla[2])
-            this.courses.set('rooms', bla.slice(3))
-            console.log(this.courses)
-            return courses
+            let word = item.split('\t')
+            const course = new Course (word[0], word[1], word[2], word.slice(3))
+            return course
         })
 
-        return courseList
+        //add to courses map
+        for (var stepper in courseList) {
+            if(courseList[stepper].code !== '' || undefined) {
+                this.courses.set(courseList[stepper].code, courseList[stepper])
+            }
+        }
+    }
+
+    addStudents (txt) {
+        let msg = 'Students: ' + txt.length + ' chars, ';
+        let a = txt.split('\n')
+        msg += a.length + ' lines'
+        console.log(msg)
+
+        //create student object
+        const studentList = a.map(item => {
+            let word = item.split('\t')
+            const student = new Student (word[0], word[1], word[2])
+
+            //find the courses in map which takes student
+            for (var stepper in word.slice(3)){
+                student.courses.push(this.courses.get(word.slice(3)[stepper]))
+            }
+
+            return student
+        })
+
+        //add to students map
+        for (var stepper in studentList) {
+            if(studentList[stepper].id !== '' | undefined) {
+                this.students.set(studentList[stepper].id, studentList[stepper])
+            }
+        }
+    }
+
+    toString (txt) {
+        return console.log(txt)
     }
 }
