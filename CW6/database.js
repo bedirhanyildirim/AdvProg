fd00343d9d0f58814be94b6d40f25ca23243f5bf
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-11-14 16:13:06 +0300

    ClassWork 6

---
+++ CW6/database.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor (id, name, gpa, courses) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+        this.courses = courses;
+    }
+    toString () {
+        return this.id+'';
+    }
+}
