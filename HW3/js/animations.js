dc18bda60ab05bffd12b3d63112dba2fa9477170
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-11-19 00:24:18 +0300

    hw3 updated

--- HW3/js/animations.js
+++ HW3/js/animations.js
@@ -1,5 +1,6 @@
 //when page is ready
 window.onload = function() {
+    //Sample 1
     document.getElementsByClassName("item-1")[0].animate([
         // keyframes
         { transform: 'translateY(0px)' }, 
@@ -11,4 +12,28 @@ window.onload = function() {
         direction: "alternate",
         easing: 'ease-in-out'
     });
+
+    //Sample 2
+    sampleTwoAnimation(1000)
 };
+
+function sampleTwoAnimation (value) {
+    value = Number(value)
+    document.getElementsByClassName("run")[0].animate([
+        {transform: 'translateX(0)'},
+        {transform: 'translateX(300px)'}
+        ], {
+        duration: value,
+        iterations: Infinity,
+        direction: "alternate"
+    });
+
+    document.getElementsByClassName("sonic")[0].animate([
+        {transform: 'rotate(0)'},
+        {transform: 'rotate(360deg)'}
+        ], {
+        duration: value,
+        iterations: Infinity,
+        direction: "alternate"
+    });
+}
