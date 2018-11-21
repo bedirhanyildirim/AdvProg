d8d9740f1ff5042261c15db8f4b1b3889eb13a73
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-11-21 18:02:34 +0300

    CW7 updated

--- sw.js
+++ sw.js
@@ -1,15 +1,31 @@
-const CACHE ='JS'
+const CACHE ='AdvProg'
+const FILES = ['/JS/CW7/index.html', '/JS/CW7/database.js', '/JS/HW3/index.html']
+
 function installCB(e) {
-  console.log('install oldu', e.request);
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
 }
 self.addEventListener('install', installCB)
+
+function cacheCB(e) { //cache first
+  let req = e.request
+  e.respondWith(
+    caches.match(req)
+    .then(r1 => r1 || fetch(req))
+    .catch(console.log)
+  )
+}
+self.addEventListener('fetch', cacheCB)
 
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  }) 
+  })
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
