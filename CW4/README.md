38d2bb37f5cb08256a0a0e24eb8a1d7ee2f714d1
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-11-25 22:08:06 +0300

    directory changed

--- CW4/README.md
+++ CW4/README.md
@@ -1,4 +1,4 @@
 # Advanced Programming with JavaScript
 In 4. week of class work we made practice about class.
 
-[Click](https://bedirhanyildirim.github.io/AdvProg/index.html) for public view.
+[Click](https://bedirhanyildirim.github.io/AdvProg/CW4/index.html) for public view.
---
+++ CW4/sw.js
@@ -0,0 +1,29 @@
+const CACHE ='AdvProg'
+const FILES = ['/AdvProg/CW7/index.html', '/AdvProg/HW1/index.html', '/AdvProg/HW2/index.html', '/AdvProg/HW3/index.html', 'https://github.com/bedirhanyildirim/AdvProg/']
+
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('JS', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
